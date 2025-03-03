2a685cdec683bf2fd65ff1b254d0135d0f628c65
seb-cheneb <bucovala.sebastian@protonmail.com>
2025-03-03 22:46:53 +0200

    authentication 1.0.0

--- darmok/authentication/views.py
+++ darmok/authentication/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import DarmokUser
 
 # Create your views here.
+def landing_page(request):
+    # retrieve all objects
+    users = DarmokUser.objects.all()
+
+    # Pagination with 3 posts per page
+    # paginator = Paginator(post_list, 3)
+    # get the page number from the url '?page=PAGE_NUMBER'
+    # page_number = request.GET.get('page', 1)
+    # check if page exists
+    # try:
+    #     posts = paginator.page(page_number)
+    # except PageNotAnInteger:
+    #     # If page_number is not an integer get the first page
+    #     posts = paginator.page(1)
+    # except EmptyPage:
+    #     # If page_number is out of range get last page of results
+    #     posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'forum/post/list.html', )
